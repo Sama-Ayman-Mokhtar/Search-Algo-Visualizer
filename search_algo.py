ed5e31fec8f4420de5def3e715f13f90ac5f2412
Sama-Ayman-Mokhtar <sama2ayman2@gmail.com>
2021-04-09 23:57:11 +0200

    FIXED accepts any node order & type; AND added path animation

--- search_algo.py
+++ search_algo.py
@@ -7,8 +7,9 @@ from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 #####################################################################################################
 #                                         N O T E S                                                #
 ####################################################################################################
-#input from user should be entered in a way such that smaller numbers come first
-#TO_DO : when adding cycles code repeat nodes
+#TO_DO : Take input from user and parse it
+#TO_DO : Print solution in result label
+#TO_DO : Di_Graph
 '''
 1 2 3 4
 4 5
@@ -23,75 +24,135 @@ from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 21 22
 23 24
 '''
+
+class Node:
+    def __init__(self, value, parent=None):
+        self.value = value
+        self.parent = parent
+    def __str__(self):
+        return str(self.value)
 #####################################################################################################
 #                                        START OF DFS                                              #
 ####################################################################################################
 class DFS:
-    def __init__(self,user_input):
-        self._adj = []
-        for x in user_input:
-            self._adj.append(list(map(int,x.split(' '))))
-        print(user_input)
-        print(self._adj)
-        self._edg = []
-        count = 0
-        for lst in self._adj:
-            for x in lst:
-                print([count, x])
-                self._edg.append([count, x])
-            count += 1
+    def __init__(self, user_input):
+        '''
+        self.graph = {'A': ['B', 'D'],
+                'B': ['C'],
+                'D': ['C'],
+                }
+        '''
+        self.graph = {0: [1, 2, 3, 4],
+                      1: [4, 5],
+                      2: [6, 7],
+                      3: [8, 9],
+                      4: [10, 11],
+                      5: [12, 13],
+                      6: [14, 15],
+                      7: [16, 17],
+                      8: [18],
+                      9: [19, 20],
+                      10: [21, 22],
+                      11: [23, 24],
+                      }
         self._Gr = nx.Graph()
-        self._Gr.add_edges_from(self._edg)
-        self._visited = ['false'] * self._Gr.number_of_nodes()
-        self._l=[]
-        self._colors=['blue'] * self._Gr.number_of_nodes()
+        for key_node in self.graph:
+            self._Gr.add_node(key_node)
+            print(self.graph[key_node])
+            for adj in self.graph[key_node]:
+                self._Gr.add_node(adj)
+                self._Gr.add_edge(key_node, adj)
+        self._l = []
+        self.shortestPath = []
+        self._colors = ['blue'] * self._Gr.number_of_nodes()
+        # self._layout = nx.spring_layout(self._Gr,scale=30, k=1/math.sqrt(self._Gr.order()))
         self._layout = nx.spring_layout(self._Gr)
-        temp = self._Gr.number_of_nodes() - len(self._adj)
-        for i in range(temp):
-            self._adj.append([])
-        print("TEMP",temp)
 
-    def display(self):
-        print(self._adj)
+    def trace(self, goal):
+        if goal.parent is None:
+            print(goal.value, end=" ")
+            self.shortestPath.append(goal.value)
+            return
+        self.trace(goal.parent)
+        self.shortestPath.append(goal.value)
+        print(" -> ", goal.value, end=" ")
 
-    def dfs(self,node):
-        self._visited[node] = 'ture'
-        #or you may use a stack
-        if node not in self._l :
-            self._l.append(node)
-        print(node)
-        for x in self._adj[node]:
-                self.dfs(int(x))
+    def dfs(self, init, goal):
+        visited = []
+        stack = []
+        stack.append(Node(init))
+        if init is goal:
+            print("Initial is goal")
+            return
+        while stack:
+            curr_node = stack.pop()
+            visited.append(curr_node.value)
+            if curr_node.value not in self._l:
+                self._l.append(curr_node.value)
+                print("visitied ", visited)
+            for adj in self.graph.get(curr_node.value) or []:
+                # if not in stack (but node vs value)
+                # https://stackoverflow.com/questions/55042460/comparing-user-defined-objects-in-two-lists
+                print("SSSSS")
+                if adj not in visited:
+                    print("ERE")
+                    temp = Node(adj, curr_node)
+                    if adj is goal:
+                        self._l.append(goal)
+                        self.trace(temp)
+                        print()
+                        return
+                    stack.append(temp)
+            for node in stack:
+                print(node.value, end=" ")
+            print()
+        print("Goal not found")
+        return
 
-    def update(self,frames, a):
+    def update(self, frames, a):
         a.clear()
-        self._colors[self._l[frames]] = 'orange'
-        # l.remove(0)
-        print(self._l[frames])
-        print(self._l)
-        nx.draw_networkx(self._Gr, pos=self._layout, node_color=self._colors, ax=a)
-        # Set the title
-        a.set_title("Frame {}".format(frames))
-        if frames == len(self._l)-1:
+        if (frames < len(self._l)):
+            # print(self._Gr.node[self._l[frames]])
+            i = 0
+            for node in self._Gr.nodes:
+                print(node, " ", self._l[frames])
+                if node is self._l[frames]:
+                    break
+                i += 1
+            self._colors[i] = 'orange'
+           # print(self._l[frames])
+           # print(self._l)
+            nx.draw_networkx(self._Gr, pos=self._layout, node_color=self._colors, ax=a)
+            # Set the title
+            a.set_title("Frame {}".format(frames))
+        else:
+            a.clear()
+            i = 0
+            for node in self._Gr.nodes:
+                print(node, " ", self.shortestPath[frames - len(self._l)])
+                if node is self.shortestPath[frames - len(self._l)]:
+                    break
+                i += 1
+            self._colors[i] = 'red'
+            nx.draw_networkx(self._Gr, pos=self._layout, node_color=self._colors, ax=a)
+            a.set_title("Frame {}".format(frames))
+
+        if frames == len(self._l + self.shortestPath) - 1:
             self._colors = ['blue'] * self._Gr.number_of_nodes()
 
     def anim(self):
         fig = plt.Figure(figsize=(5, 4))
         ax = fig.add_subplot(111)
         plt.axis('off')
-        #nx.draw_networkx(self._Gr, pos=self._layout, ax=ax)
+        # nx.draw_networkx(self._Gr, pos=self._layout, ax=ax)
         canvas = FigureCanvasTkAgg(fig, frm_left)
         canvas.draw()
         canvas.get_tk_widget().grid(row=1, column=0)
         print(len(self._l))
-        ani = animation.FuncAnimation(fig, self.update, frames= len(self._l), interval=400, fargs={ax})
+        ani = animation.FuncAnimation(fig, self.update, frames=len(self._l + self.shortestPath), interval=400,fargs={ax})
         canvas = FigureCanvasTkAgg(fig, frm_left)
         canvas.draw()
         canvas.get_tk_widget().grid(row=1, column=0)
-       # plt.show()
-        #ani.save('animation_1.gif', writer='imagemagick')
-        #plt.show()
-
 
 
 #####################################################################################################
@@ -103,13 +164,13 @@ class DFS:
 #####################################################################################################
 #                               START OF BUTTON EVENT LISTENER                                      #
 ####################################################################################################
-def onClickRun(user_txtBox,type_var):
+
+def onClickRun(user_txtBox, type_var):
   input = user_txtBox.get("1.0", "end-1c").split('\n')
   if(type_var == 1):
       dfs_inst = DFS(input)
-      dfs_inst.display()
-      #dfs_inst.draw_graph()
-      dfs_inst.dfs(0)
+      dfs_inst.dfs(0,15)
+    #  dfs_inst.dfs('A','C')
       dfs_inst.anim()
       print(dfs_inst._l)
   print(input , 'whatever' , type_var)
